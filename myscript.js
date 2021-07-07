8be56d4ef0cedeead46d944d1fb0bc1bd8ac566f
Cobboldgit <augustinecobbold6@gmail.com>
2021-07-07 14:30:01 +0000

    added myscript.html and myscript.js

---
+++ myscript.js
@@ -0,0 +1,16 @@
+
+console.log(2 + 2)
+console.log(2 - 2)
+console.log(2 * 2)
+console.log(2 % 2)
+console.log(2 / 2)
+
+var addone = 90
+console.log(addone++)
+console.log(addone)
+
+
+var add = 100
+console.log(++add)
+
+console.log('a' + 'b')
